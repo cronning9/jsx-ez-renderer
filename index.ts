0dd5931a7603ce488971e635873e23f4da3ef402
ooronning <christopher.a.ronning@gmail.com>
2019-09-08 21:26:46 -0400

    various reconfigurations

--- index.ts
+++ index.ts
@@ -2,15 +2,17 @@ interface RunProps {
   [key: string]: string;
 }
 
-export default class JSXEngine {
-  /**
-   * When reading JSX input, the properly-configured TypeScript compiler
-   * will output calls to this function, passing JSX arguments to the proper position.
-   * 
-   * This function should output a string of valid HTML.
-   */
-  // TODO: figure out how to type a component identifier
-  public static run = (type: string, props: RunProps | null, children: any): string => {
-    return 'test';
-  }
+/**
+ * When reading JSX input, the properly-configured TypeScript compiler
+ * will output calls to this function, passing JSX arguments to the proper position.
+ * 
+ * This function should output a string of valid HTML.
+ */
+// TODO: figure out how to type a component identifier
+const run = (type: string, props: RunProps | null, children: any): string => {
+  return 'test';
+}
+
+export default {
+  run,
 }
