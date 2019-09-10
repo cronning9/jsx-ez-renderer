90ee018975e94434f746fb7517120902eeaab24d
ooronning <christopher.a.ronning@gmail.com>
2019-09-09 20:46:24 -0400

    move current tests into one module for intrinsic elements

--- __test__/JSXEngine.test.ts
+++ __test__/JSXEngine.test.ts
@@ -1,15 +1,17 @@
 import JSXEngine from "..";
 
-test('outputs a single div with no content or properties', () => {
-  expect(JSXEngine.run('div', null, null).htmlString).toBe('<div></div>');
-});
+describe('intrinsic elements', () => {
+  test('outputs a single div with no content or properties', () => {
+    expect(JSXEngine.run('div', null, null).htmlString).toBe('<div></div>');
+  });
 
-// TODO: make this pass
-test('outputs an HTML element with a nested HTML element', () => {
-  expect(JSXEngine.run('div', null, JSXEngine.run('canvas', null))).toBe(
-    '<div>\n' +
-    '  <canvas></canvas>\n' +
-    '</div>'
+  // TODO: make this pass
+  test('outputs an HTML element with a nested HTML element', () => {
+    expect(JSXEngine.run('div', null, JSXEngine.run('canvas', null)).htmlString).toBe(
+      '<div>\n' +
+      '  <canvas></canvas>\n' +
+      '</div>'
 
-  );
+    );
+  });
 })
