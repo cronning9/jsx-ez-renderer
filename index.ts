6e36c06862379b3f9bfa5e7a0ec50d4081535a57
ooronning <christopher.a.ronning@gmail.com>
2019-11-03 16:07:34 -0500

    finish it wrongly

--- index.ts
+++ index.ts
@@ -3,14 +3,13 @@ import JSXElement, { JSXChildren } from './lib/JSXElement';
 
 export interface IntrinsicElementAttributes {
   [key: string]: string | boolean | number;
-  // banana: string;
 }
 
-export interface Component<P = undefined> {
-  (props: P): JSXElement
+export interface Component<P = any> {
+  (props: P): JSXElement<P>
 }
 
-type Identifier = string | Component<unknown>;
+type Identifier = string | Component;
 
 /**
  * When reading JSX input, the properly-configured TypeScript compiler
@@ -22,27 +21,35 @@ function run<P extends object>(
   identifier: Component<P>,
   props: P | null,
   ...children: JSXChildren
-): JSXElement;
+): JSXElement<P>;
 function run(
   identifier: string,
   props: IntrinsicElementAttributes | null,
   ...children: JSXChildren
-): JSXElement;
-function run<P = undefined>(
+): JSXElement<IntrinsicElementAttributes>;
+function run<P>(
   identifier: Identifier,
   props: P | IntrinsicElementAttributes | null,
   ...children: JSXChildren
-): JSXElement {
+): JSXElement<P | IntrinsicElementAttributes> {
+  // check for users who don't use TypeScript
+  if (!identifierIsString(identifier) && !identifierIsComponent(identifier)) {
+    throw new InvalidElementError(`Identifier ${identifier} must be a string or a function component`);
+  }
   if (identifierIsString(identifier) && !IntrinsicElements.includes(identifier)) {
     throw new InvalidElementError(`${identifier} is not a valid JSX element.`);
   }
-
   if (identifierIsComponent<P>(identifier)) {
-    const element = identifier(props);
-    return new JSXElement(element, props, ...children);
+    // TODO: figure out hwo to narrow down to P or IntrinsiceElementAttributes without casting
+    const element = identifier(props as P);
+    return new JSXElement(element, props as P, ...children);
   }
 
-  return new JSXElement(identifier, props, ...children);
+  return new JSXElement(
+    identifier as string,
+    props as IntrinsicElementAttributes,
+    ...children
+  );
 }
 
 function identifierIsString(identifier: Identifier): identifier is string {
@@ -51,7 +58,7 @@ function identifierIsString(identifier: Identifier): identifier is string {
 
 function identifierIsComponent<P>(identifier: string | Component<P>): identifier is Component<P> {
   const argsAreCorrect = (id: Component<P>) => id.arguments.length === 1 || id.arguments.length === 0;
-  if (!(typeof identifier === 'function') || !argsAreCorrect(identifier)) {
+  if (typeof identifier !== 'function' || !argsAreCorrect(identifier)) {
     return false;
   }
 
